1579bfd3b766a85ffdb81cc08053139362ae9644
Nikhil Jose <nikhiljose12345@gmail.com>
2022-05-03 16:30:05 +0530

    heroku config

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const router=require('./routes.js')
 
 app.use(router)
 
-// const port=process.env.PORT;
-const port=5000;
+const port=process.env.PORT;
+// const port=5000;
 app.listen(port,()=>{
     console.log(`Server running on Port ${port}`);
 })
