c682e6e14c0007af019eeaf8535cf1c01bf94d23
Nikhil Jose <nikhiljose12345@gmail.com>
2022-08-15 12:26:21 +0530

    minor refactor

--- controllers.js
+++ controllers.js
@@ -7,20 +7,15 @@ const extractApi=async(req,res)=>{
     .catch(err=>{
         res.status(400).send("Check URL my friend")
     })
-    // console.log(images)
     res.send(images);
-
 }
 
 
 async function getImages(url){
-    // let imgArray=[]
+
     // url="https://cdn.discordapp.com/attachments/970770954139607150/970945525081866270/nikiljos.zip"
-    //get the zip file to client
     let images=await axios.get(url, { responseType: 'arraybuffer' }).then(async (res) => {
-    // load contents into jszip and create an object
-    // console.log(new Blob([res.data],{ type: 'application/zip' }));
-        // console.log(res.data)
+        // load contents into jszip and create an object
         let content= await jszip.loadAsync(res.data).then(async (zip) => {
 
             const zipObj = zip
@@ -35,16 +30,11 @@ async function getImages(url){
                     if(img._data.compressedContent){
                         console.log(key)
                         let fileType=mime.lookup(key)
-                        // console.log(fileType)
-                        
-                        // console.log(string64)
                         if(fileType.startsWith("image")){
                             await img.async("base64").then(function (string64) {
                                 //send this image to a remote server as if it were a file
                                 let imgURL=`data:${fileType};base64,${string64}`
-                                // console.log(imgURL)
                                 imgArray.push(imgURL)
-                                // console.log(imgArray)
 
                             });
                             
@@ -54,13 +44,9 @@ async function getImages(url){
                     
                 }
             }
-            // console.log(imgArray)
             return imgArray
         })
-        // console.log(content)
-        return content
-        
-        
+        return content        
     })
     .catch(err=>{
         throw err;
